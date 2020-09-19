593f5d4b9136ddafd91ba8c409072011ce9b536d
gitz10 <71571591+gitz10@users.noreply.github.com>
2020-09-19 17:20:28 -0500

    Update script.js
    integrated openweathermap api to fetch weather details of a cityname entered in text box on clicking "Fetch Weather" button

--- view/js/script.js
+++ view/js/script.js
@@ -1,8 +1,25 @@
 function Weather() {
 }
 
-Weather.prototype.fetchResults = function(val) {
-  
+Weather.prototype.fetchResults = function() {
+
+    let city = weatherApp.$city.value;
+    let key = '711a0af6cea834b4455057aa00cdbccf';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city+ '&appid=' + key)
+        .then(function(resp) { return resp.json() }) // Convert data to json
+        .then(function(data) {
+          console.log(data);
+          console.log(data.name);
+          console.log(data.weather[0].description);
+          document.getElementById('selectedCity').innerHTML = data.name;
+          document.getElementById('selctedWeather').innerHTML = data.weather[0].description;
+          document.getElementById('selectedStatus').innerHTML = 'Wind speed: '+data.wind.speed;
+        })
+        .catch(function() {
+          // catch any errors
+        });
+
+
 }
 
 Weather.prototype.onKeyup = function(e) {
@@ -18,7 +35,27 @@ Weather.prototype.updateSuggestions = function() {
 }
 
 Weather.prototype.reset = function() {
-  
+  weatherApp.init();
+  weatherApp.$city.value = '';
+  document.getElementById('selectedCity').innerHTML = '';
+  document.getElementById('selctedWeather').innerHTML = '';
+  document.getElementById('selectedStatus').innerHTML = '';
+
+}
+/*const debounceEvent = (callback, time = 250, interval) =>
+    (...args) => {
+      clearTimeout(interval);
+      interval = setTimeout(() => callback(...args), time);
+    }*/
+Weather.prototype.debounce = (callback, delay = 250) => {
+  let timeoutId
+  return (...args) => {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => {
+      timeoutId = null
+      callback(...args)
+    }, delay)
+  }
 }
 
 Weather.prototype.init = function() {
@@ -31,6 +68,7 @@ Weather.prototype.init = function() {
   this.$selctedWeather = document.getElementById('selctedWeather');
   this.$selectedStatus = document.getElementById('selectedStatus');
   this.$resetBtn = document.getElementById('resetBtn');
+  this.$fetchWeatherBtn = document.getElementById('fetchBtn');
   this.$city.addEventListener('keyup', this.onKeyup.bind(this));
   this.$resetBtn.addEventListener('click', this.reset.bind(this));
 }
